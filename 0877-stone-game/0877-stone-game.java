1e251bb7c7e3111201a999bb6ae8f87c36e62941
Yash Bhatia <bhatiayash132@gmail.com>
2026-08-04 00:36:15 +0530

    Time: 0 ms (100%), Space: 43 MB (62.6%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,23 @@
 class Solution {
-    public boolean stoneGame(int[] piles) {
-        return true;
+    public boolean stoneGame(int[] arr) {
+        // return true;
+        int n = arr.length;
+        int i = 0;
+        int j = n-1;
+        int a = 0;
+        int b = 0;
+        boolean flag = false;
+        while(i < j){
+            if(!flag){
+                a+= Math.max(arr[i], arr[j]);
+            }else{
+                b+=Math.min(arr[i],arr[j]);
+            }
+            i++;
+            j--;
+        }
+
+        if(a > b) return true;
+        else return false;
     }
 }
