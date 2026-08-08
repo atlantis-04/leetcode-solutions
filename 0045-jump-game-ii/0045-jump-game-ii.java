22c5fa7d47b6cbeb4c20c007dd17efde11e8870f
Yash Bhatia <bhatiayash132@gmail.com>
2026-08-08 15:22:30 +0530

    Time: 1 ms (99.76%), Space: 47.2 MB (58.19%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int jump(int[] arr) {
+    int n = arr.length;
+
+    int jumps = 0;
+    int l = 0;
+    int r = 0;
+
+    while (r < n - 1) {
+        int farthest = 0;
+
+        for (int i = l; i <= r; i++) {
+            farthest = Math.max(farthest, i + arr[i]);
+        }
+
+        l = r + 1;
+        r = farthest;
+        jumps++;
+    }
+
+    return jumps;
+}
+}
