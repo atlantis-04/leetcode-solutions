4cf429e69e647e2ac1e122a9c70d178915b7bc8f
Yash Bhatia <bhatiayash132@gmail.com>
2026-08-18 23:24:26 +0530

    Time: 1 ms (14.3%), Space: 42.3 MB (13.06%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,20 +1,24 @@
 class Solution {
-    int[][] dp;
-    public int solve(int m, int n,int i, int j ){
-        if(i >=m || j >=n) {return 0;}
-        if(i == m-1 && j == n-1) {return 1;}
-        if(dp[i][j] != 0) {
-            return dp[i][j];
-        }
-
-        int right = solve(m,n,i ,j+1);
-        int down = solve(m,n,i+1,j);
-
-        dp[i][j] = right + down;
-        return dp[i][j];
-    }
     public int uniquePaths(int m, int n) {
-        dp = new int[m][n];
-        return solve(m,n,0,0);
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(i==0 && j == 0){
+                    continue;
+                }
+                int up = 0;
+                int left = 0;
+
+                if (i > 0)
+                    up = dp[i - 1][j];
+
+                if (j > 0)
+                    left = dp[i][j - 1];
+                
+                dp[i][j] = up + left;
+            }
+        }
+         return dp[m - 1][n - 1];
     }
 }
