8f66f1d1a6610879b014469a2ca3ec55c627ed7e
Yash Bhatia <bhatiayash132@gmail.com>
2026-08-27 22:38:23 +0530

    Time: 0 ms (100%), Space: 42.2 MB (38.93%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int integerBreak(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+
+        int q = n / 3;
+        int r = n % 3;
+
+        if (r == 0) {
+            return (int) Math.pow(3, q);
+        } 
+        else if (r == 1) {
+            return (int) (Math.pow(3, q - 1) * 4);
+        } 
+        else {
+            return (int) (Math.pow(3, q) * 2);
+        }
+    }
+}
