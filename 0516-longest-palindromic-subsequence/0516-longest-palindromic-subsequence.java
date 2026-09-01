fa24cd20450b7229cd79cd2691f7259184232db2
Yash Bhatia <bhatiayash132@gmail.com>
2026-09-01 09:58:41 +0530

    Time: 56 ms (42.19%), Space: 63.6 MB (70.77%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,36 +1,49 @@
-import java.util.Arrays;
-
 class Solution {
-    private int[][] dp;
+    public int func(String s1, String s2) {
+        // Length of first string
+        int n = s1.length();
+        // Length of second string
+        int m = s2.length();
 
-    private int solve(String s, int i, int j) {
+        // 2D DP array: dp[i][j] stores LCS length of s1[0..i-1] and s2[0..j-1]
+        int[][] dp = new int[n + 1][m + 1];
 
-        if (i > j)
-            return 0;
-
-        if (i == j)
-            return 1;
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        if (s.charAt(i) == s.charAt(j)) {
-            return dp[i][j] = 2 + solve(s, i + 1, j - 1);
+        // Initialize first row to 0 (when one string is empty)
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        // Initialize first column to 0 (when the other string is empty)
+        for (int i = 0; i <= m; i++) {
+            dp[0][i] = 0;
         }
 
-        return dp[i][j] = Math.max(
-                solve(s, i + 1, j),
-                solve(s, i, j - 1)
-        );
+        // Fill DP table
+        for (int ind1 = 1; ind1 <= n; ind1++) {
+            for (int ind2 = 1; ind2 <= m; ind2++) {
+                // If current characters match, add 1 to the LCS of previous characters
+                if (s1.charAt(ind1 - 1) == s2.charAt(ind2 - 1)) {
+                    dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+                }
+                // If they don't match, take the max by ignoring one character from either string
+                else {
+                    dp[ind1][ind2] = Math.max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+                }
+            }
+        }
+
+        // LCS length is stored in the bottom-right cell
+        return dp[n][m];
+    }
+
+    public int longestPalinSubseq(String s) {
+        // Reverse the original string
+        String t = new StringBuilder(s).reverse().toString();
+
+        // LPS is just the LCS between the string and its reverse
+        return func(s, t);
     }
 
     public int longestPalindromeSubseq(String s) {
-        int n = s.length();
-        dp = new int[n][n];
-
-        for (int[] row : dp)
-            Arrays.fill(row, -1);
-
-        return solve(s, 0, n - 1);
+        return longestPalinSubseq(s);
     }
 }
